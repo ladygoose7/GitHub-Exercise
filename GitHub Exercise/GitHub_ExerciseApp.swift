050790b36fbd534a5f7928adc93e67d327c935fc
Brandon <brandon.c.sheehan@gmail.com>
2023-05-07 14:54:21 -0700

    Initial Commit

---
+++ GitHub Exercise/GitHub_ExerciseApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHub_ExerciseApp.swift
+//  GitHub Exercise
+//
+//  Created by Brandon Sheehan on 5/7/23.
+//
+
+import SwiftUI
+
+@main
+struct GitHub_ExerciseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
